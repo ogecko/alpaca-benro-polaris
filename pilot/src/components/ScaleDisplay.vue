6ebe2459598bfcfb28ec11bb50728c56fa692994
David Morrison <jdmorriso@gmail.com>
2025-08-29 23:56:37 +1000

    fix(pilot): major and minor ticks, color customisation of scales

--- pilot/src/components/ScaleDisplay.vue
+++ pilot/src/components/ScaleDisplay.vue
@@ -12,13 +12,11 @@ import { axisBottom } from 'd3-axis'
 import { select } from 'd3-selection'
 import { transition } from 'd3-transition'
 import { interpolate } from 'd3-interpolate'
-// import { zoom } from 'd3-zoom'
 import { easeCubicOut } from 'd3-ease'
 import type { ScaleLinear } from 'd3-scale'
-
-// import type { Axis } from 'd3-axis'
-// import type { Selection } from 'd3-selection'
-// import type { NumberValue } from 'd3-scale'
+import type { Selection } from 'd3-selection';
+import type { Transition } from 'd3-transition';
+import type { BaseType } from 'd3-selection';
 
 export type ScaleDomainType =
 	| 'linear_360'
@@ -48,6 +46,114 @@ const renderKey = computed(() => `${props.domain}-${props.scaleStart}-${props.sc
 onMounted(renderScale)
 watch(renderKey, renderScale)
 
+function angleInterp(oldScale: ScaleLinear<number, number>, newScale: ScaleLinear<number, number>) {
+  return (d: number) => interpolate(oldScale(d), newScale(d));
+}
+
+function labelTransformTween(interpFn: (t: number) => number, radius: number) {
+  return function (t: number) {
+    const angle = interpFn(t);
+    const x = radius * Math.cos(angle * Math.PI / 180) * 1.1;
+    const y = radius * Math.sin(angle * Math.PI / 180) * 1.1;
+    return `rotate(${-angle}, ${x}, ${y}) rotate(${angle})`;
+  };
+}
+
+function joinLines(group: Selection<SVGGElement, unknown, null, undefined>, 
+	ticks: number[], oldScale: ScaleLinear<number, number>, 
+	newScale: ScaleLinear<number, number>, 
+	radius: number,
+    tRaw: Transition<BaseType, number, SVGGElement, unknown>,
+	{
+		key = 'line',
+		x1 = 0.9,
+		x2 = 0.98,
+		thickness = 1.0,
+		color = 'white'
+	}: {
+		key?: string,
+		x1?: number;
+		x2?: number;
+		thickness?: number;
+		color?: string;
+	} = {} // default to empty object
+) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const t = tRaw as Transition<BaseType, any, any, any>
+  const interp = angleInterp(oldScale, newScale);
+
+  group.selectAll<SVGLineElement, number>(`.${key}`)
+    .data(ticks, d => d)
+    .join(
+      enter => enter.append('line')
+	    .attr('class', key)
+        .attr('stroke', color)
+		.attr('stroke-width', thickness)
+        .attr('x1', radius * x1)
+        .attr('x2', radius * x2)
+        .attr('y1', 0)
+        .attr('y2', 0)
+        .attr('opacity', 0)
+        .attr('transform', d => `rotate(${oldScale(d)})`)
+        .transition(t)
+        .attr('opacity', 1)
+        .attrTween('transform', d => t => `rotate(${interp(d)(t)})`),
+
+      update => update.transition(t)
+        .attr('opacity', 1)
+        .attrTween('transform', d => t => `rotate(${interp(d)(t)})`),
+
+      exit => exit.transition(t)
+        .attr('opacity', 0)
+        .remove()
+        .attrTween('transform', d => t => `rotate(${interp(d)(t)})`)
+    );
+}
+
+function joinLabels(group: Selection<SVGGElement, unknown, null, undefined>, 
+	ticks: number[], oldScale: ScaleLinear<number, number>, 
+	newScale: ScaleLinear<number, number>, 
+	radius: number, 
+    tRaw: Transition<BaseType, number, SVGGElement, unknown>,
+		{
+		key = 'text',
+		color = 'white'
+	}: {
+		key?: string,
+		color?: string;
+	} = {} // default to empty object
+) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const t = tRaw as Transition<BaseType, any, any, any>
+  const interp = angleInterp(oldScale, newScale);
+
+  group.selectAll<SVGTextElement, number>(`.${key}`)
+    .data(ticks, d => d)
+    .join(
+      enter => enter.append('text')
+ 	    .attr('class', key)
+        .attr('fill', color)
+        .attr('text-anchor', 'middle')
+        .attr('dominant-baseline', 'middle')
+        .attr('x', radius * 1.1)
+        .attr('y', 0)
+        .text(d => d.toString())
+        .attr('opacity', 0)
+        .attr('transform', d => `rotate(${oldScale(d)})`)
+        .transition(t)
+        .attr('opacity', 1)
+        .attrTween('transform', d => t => labelTransformTween(interp(d), radius)(t)),
+
+      update => update.transition(t)
+        .attrTween('transform', d => t => labelTransformTween(interp(d), radius)(t)),
+
+      exit => exit.transition(t)
+        .attr('opacity', 0)
+        .remove()
+        .attrTween('transform', d => t => labelTransformTween(interp(d), radius)(t))
+    );
+}
+
 function renderLinearScale() {
 	if (!linearGroup.value) return
 
@@ -63,112 +169,24 @@ function renderLinearScale() {
 
 let prevScale: ScaleLinear<number, number> | undefined;
 function renderCircularScale() {
-	if (!circularGroup.value) return
+  if (!circularGroup.value) return;
 
-	const radius = width / 2 - 60
-	const newScale = scaleLinear()
-		.domain([props.pv - props.scaleRange / 2, props.pv + props.scaleRange / 2])
-		.range([-10, 190]);
-	const oldScale = prevScale ?? newScale; // fallback on first render
-	prevScale = newScale; // stash for next time
+  const radius = width / 2 - 60;
+  const newScale = scaleLinear()
+    .domain([props.pv - props.scaleRange / 2, props.pv + props.scaleRange / 2])
+    .range([-10, 190]);
+  const oldScale = prevScale ?? newScale;
+  prevScale = newScale;
 
-	const ticks: number[] = newScale.ticks(10)
-	const group = select(circularGroup.value)
-	const t = transition().duration(200).ease(easeCubicOut)
+  const bticks = newScale.ticks(10);
+  const sticks = newScale.ticks(50);
+  const group = select(circularGroup.value);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const t = transition().duration(200).ease(easeCubicOut) as Transition<BaseType, any, any, any>;
 
-	// Bind data to lines
-	const lines: d3.Selection<SVGLineElement, number, SVGGElement, unknown> =
-		group.selectAll<SVGLineElement, number>('line')
-			.data(ticks, (d: number) => d);
-
-	// const tickAngleCache = new WeakMap<SVGLineElement, number>();
-
-	lines.join(
-		enter => enter.append('line')
-			.attr('stroke', 'white')
-			.attr('x1', radius * 0.9)
-			.attr('x2', radius * 1.0)
-			.attr('y1', 0)
-			.attr('y2', 0)
-			.attr('opacity', 0)
-			.attr('transform', d => `rotate(${oldScale(d)})`)
-			.transition(t)
-			.attr('opacity', 1)
-			.attrTween('transform', function (d) {
-				const interp = interpolate(oldScale(d), newScale(d));
-				return t => `rotate(${interp(t)})`;
-			}),
-
-		update => update.transition(t)
-			.attr('stroke', 'white')
-			.attr('opacity', 1)
-			.attrTween('transform', function (d) {
-				const interp = interpolate(oldScale(d), newScale(d));
-				return t => `rotate(${interp(t)})`;
-			}),
-
-		exit => exit.transition(t)
-			.remove()
-			.attr('opacity', 0)
-			.attrTween('transform', function (d) {
-				const interp = interpolate(oldScale(d), newScale(d));
-				return t => `rotate(${interp(t)})`;
-			})
-	)
-	// Bind data to text
-	const labels: d3.Selection<SVGTextElement, number, SVGGElement, unknown> =
-		group.selectAll<SVGTextElement, number>('text')
-			.data(ticks, (d: number) => d);
-
-	labels.join(
-		enter => enter.append('text')
-			.attr('fill', 'white')
-			.attr('text-anchor', 'middle')
-			.attr('dominant-baseline', 'middle')
-			.attr('x', radius * 1.1)
-			.attr('y', 0)
-			.text(d => d.toString())
-			.attr('opacity', 0)
-			.attr('transform', d => `rotate(${oldScale(d)})`)
-			.transition(t)
-			.attr('opacity', 1)
-			.attrTween('transform', function (d) {
-				const interp = interpolate(oldScale(d), newScale(d));
-				return function (t) {
-					const angle = interp(t);
-					const x = radius * Math.cos(angle * Math.PI / 180) * 1.1;
-					const y = radius * Math.sin(angle * Math.PI / 180) * 1.1;
-					return `rotate(${-angle}, ${x}, ${y}) rotate(${angle})`;
-				};
-			}),
-
-		update => update.transition(t)
-			.attrTween('transform', function (d) {
-				const interp = interpolate(oldScale(d), newScale(d));
-				return function (t) {
-					const angle = interp(t);
-					const x = radius * Math.cos(angle * Math.PI / 180) * 1.1;
-					const y = radius * Math.sin(angle * Math.PI / 180) * 1.1;
-					return `rotate(${-angle}, ${x}, ${y}) rotate(${angle})`;
-				};
-			}),
-
-		exit => exit.transition(t)
-			.remove()
-			.attr('opacity', 0)
-			.attrTween('transform', function (d) {
-				const interp = interpolate(oldScale(d), newScale(d));
-				return function (t) {
-					const angle = interp(t);
-					const x = radius * Math.cos(angle * Math.PI / 180) * 1.1;
-					const y = radius * Math.sin(angle * Math.PI / 180) * 1.1;
-					return `rotate(${-angle}, ${x}, ${y}) rotate(${angle})`;
-				};
-			})
-
-
-	);
-
+  joinLines(group, bticks, oldScale, newScale, radius, t, { key: 'major', thickness: 3, color: 'lightskyblue' });
+  joinLines(group, sticks, oldScale, newScale, radius, t, { key: 'minor', x1: 0.9, x2: 0.94,  color: 'cornflowerblue'});
+  joinLabels(group, bticks, oldScale, newScale, radius, t, { color: 'lightCyan' });
 }
 
 
