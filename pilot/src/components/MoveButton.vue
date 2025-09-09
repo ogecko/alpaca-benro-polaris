027fbed955898e8193d48b57a3bd08e139b7c070
David Morrison <jdmorriso@gmail.com>
2025-09-09 12:51:04 +1000

    fix(pilot): no longer need to import defineProps

--- pilot/src/components/MoveButton.vue
+++ pilot/src/components/MoveButton.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineProps, defineEmits } from 'vue'
+import { ref } from 'vue'
 
 defineProps({
   icon: { type: String, default: 'mdi-plus-circle' },
