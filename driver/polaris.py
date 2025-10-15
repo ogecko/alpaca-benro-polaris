f899e64d231512db9d708ec3ce9cb5d75bdc53bd
David Morrison <jdmorriso@gmail.com>
2025-10-16 00:06:19 +1100

    fix(sync): fixed error in alignment sync (why is ra in hr and all other angles degrees!)

--- driver/polaris.py
+++ driver/polaris.py
@@ -590,8 +590,12 @@ class Polaris:
         self._azimuth = a_az
         self._pid.alpha_sp[0] = a_az
         self._pid.alpha_sp[1] = a_alt
-        self._pid.delta_sp[0] = a_ra
+        self._pid.delta_sp[0] = a_ra * 15
         self._pid.delta_sp[1] = a_dec
+        corrected_position_angle, _ = self._sm.roll2pa(a_az, a_alt, self._roll) # to preserve roll
+        self._pid.delta_sp[2] = corrected_position_angle
+        self.logger.info(f"->> Polaris: DeltaSP    RA {hr2hms(self._pid.delta_sp[0])} Dec {deg2dms(self._pid.delta_sp[1])} PA {deg2dms(self._pid.delta_sp[2])}")
+        self.logger.info(f"->> Polaris: DeltaOfft  RA {hr2hms(self._pid.delta_offst[0])} Dec {deg2dms(self._pid.delta_offst[1])} PA {deg2dms(self._pid.delta_offst[2])}")
 
         return
 
