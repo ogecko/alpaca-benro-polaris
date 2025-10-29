a230d33b6e74c02ee8271416370d569b9a1e093e
David Morrison <jdmorriso@gmail.com>
2025-10-29 22:10:01 +1100

    fix(pilot): rebuild of pilot with JNow changes for catalog and other minor changes

--- pilot/dist/spa/assets/autoconnect-Bs1gj-2G.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./device-BSVNe9Yd.js";import"./index-CatOEmX3.js";const c=async()=>{const t=o();t.alpacaHost&&t.restAPIPort&&await t.connectRestAPI()};export{c as default};
--- pilot/dist/spa/assets/use-quasar-E_dTUprs.js
+++
@@ -1,1 +0,0 @@
-import{j as a,aP as r}from"./index-CatOEmX3.js";function u(){return a(r)}export{u};
