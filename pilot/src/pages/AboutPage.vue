7bb156dba0538be9cfd03a92556012d03e2cc8b1
David Morrison <jdmorriso@gmail.com>
2025-11-11 16:41:11 +1100

    fix(ver): bumped dev_2 branch to beta5

--- pilot/src/pages/AboutPage.vue
+++ pilot/src/pages/AboutPage.vue
@@ -30,7 +30,7 @@
               <tr><th class="text-left">Component</th><th class="text-left">Version</th></tr>
             </thead>
             <tr><td>Alpaca Pilot App</td><td>v2.0.0 Beta4</td></tr>
-            <tr><td>{{ dev.alpacaServerName }}</td><td>v{{ dev.alpacaServerVersion }} Beta4</td></tr>
+            <tr><td>{{ dev.alpacaServerName }}</td><td>v{{ dev.alpacaServerVersion }} Beta5</td></tr>
             <tr><td>Benro Polaris Firmware</td><td>v{{ p.polarisswver }}</td></tr>
             <tr><td>Benro Polaris Hardware</td><td>v{{ p.polarishwver }}</td></tr>
           </q-markup-table>
