b547bc83d391177bd06d33f6e2aa77e43791e749
David Morrison <jdmorriso@gmail.com>
2025-12-10 21:55:00 +1100

    fix(pilot): rebuild pilot for typo on about page

--- pilot/dist/spa/assets/autoconnect-CChK4pP4.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./device-BB0QsvLA.js";import"./index-Bc3tU-LI.js";const c=async()=>{const t=o();t.alpacaHost&&t.restAPIPort&&await t.connectRestAPI()};export{c as default};
--- pilot/dist/spa/assets/use-quasar-B65w70zK.js
+++
@@ -1,1 +0,0 @@
-import{j as a,aQ as r}from"./index-Bc3tU-LI.js";function u(){return a(r)}export{u};
