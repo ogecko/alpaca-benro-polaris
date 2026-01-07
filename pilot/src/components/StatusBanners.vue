f2c5ae8f65641f367a9dc6346fb0e73212d1490d
David Morrison <jdmorriso@gmail.com>
2026-01-07 14:36:54 +1100

    fix(pilot): refine wording of No Astro Module banner warning

--- pilot/src/components/StatusBanners.vue
+++ pilot/src/components/StatusBanners.vue
@@ -19,7 +19,7 @@
     </div>
     <div v-else-if="isNoAstroModule" >
       <q-banner inline-actions rounded class="bg-warning" >
-        WARNING: No Astro version detected. Please attach Polaris Astro modul, then disconnect and reconnect the Polaris.
+        WARNING: No Astro version detected. Please attach Polaris Astro module, then disconnect and reconnect the Polaris.
         <template v-slot:action><q-btn v-if="isShowReconnect" flat label="Reconnect" to="/connect" /></template>
       </q-banner>
     </div>
