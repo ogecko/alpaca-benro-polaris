9d33abf538e2c04335430786cb32337a41cba54a
David Morrison <jdmorriso@gmail.com>
2026-01-16 18:22:21 +1100

    fix(pilot): Rebuild pilot with IMX462, and scroll nav region for large mosaics

--- pilot/dist/spa/assets/autoconnect-DqiruUkj.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./device-B7V9CiJc.js";import"./index-4Lh3THfB.js";const c=async()=>{const t=o();t.alpacaHost&&t.restAPIPort&&await t.connectRestAPI()};export{c as default};
---
+++ pilot/dist/spa/assets/use-quasar-BaD8cjJo.js
@@ -0,0 +1,1 @@
+import{j as a,aQ as r}from"./index-DrJQmL8B.js";function u(){return a(r)}export{u};
