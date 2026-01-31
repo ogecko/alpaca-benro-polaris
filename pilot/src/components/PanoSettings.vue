611f9d7759d23fc09bae7fa95747307d1ac570f2
Billy Buchanan <william@williambuchanan.net>
2026-01-31 08:06:53 -0500

    Allows users to set starting position for panoramas
    Added starting position parameter to UI and Config to allow users to specify starting the panorama from the top left, top right, bottom left, or bottom right panel of the panorama.  If users select starting from the right most column, the rotation calculation for the next panel is modified with a -1 multiplication to change the direction of rotation on the azimuth plane.  If users select starting from the top row, the rotation calculation for the next panel is modified with a -1 multiplication to change the direction of rotation on the altitude plane.  UI element is added to the Vue application with the four options and both the driver configuration and Vue configuration files have the  member added with a default value of 'br' (bottom right).  Documentation is updated with a new screenshot showing the added parameter in the UI and following the same style as the previous screenshot included in the documentation.

--- pilot/src/components/PanoSettings.vue
+++ pilot/src/components/PanoSettings.vue
@@ -1,91 +1,187 @@
-
-
 <template>
-    <q-card flat bordered class="q-pa-md full-width">
-        <div class="text-h6">Panorama Grid Layout</div>
-        <div class="row q-col-gutter-lg  items-center q-pt-sm">
-            <q-input class="col-2" v-bind="bindField('cols', 'Columns')" type="number" input-class="text-right" dense/>
-            <q-input class="col-2" v-bind="bindField('rows', 'Rows')" type="number" input-class="text-right" dense/>
-            <div class="col-1"></div>
-            <q-btn  class="col-1 self-end q-pt-sm" size="md"  text-color="grey-6"  flat rounded dense icon="mdi-calculator">
-              <q-popup-proxy><PanoCalculator class=""/></q-popup-proxy>
-            </q-btn>
-            <q-input class="col-3" v-bind="bindField('hstep', 'Horizontal Step', '°')" type="number" input-class="text-right" dense/>
-            <q-input class="col-3" v-bind="bindField('vstep', 'Vertical Step', '°')" type="number" input-class="text-right" dense/>
-        </div>
-        <div class="row q-col-gutter-lg  items-center q-pt-lg">
-            <q-select
-              class="col-6 q-pt-none" label="Panel Order" emit-value map-options
-              v-model="cfg.order" @update:model-value="v => putdb({ order: v })"
-              :options="panoOrderOptions"
-            />
-            <q-select
-              class="col-6 q-pt-none" label="Orientation and Tracking" emit-value map-options
-              v-model="cfg.track" @update:model-value="v => putdb({ track: v })"
-              :options="panoTrackingOptions"
-            />
-        </div>
-        <div class="text-h6 q-pt-lg">Panorama Grid Positioning</div>
-        <div class="row q-col-gutter-lg  items-center q-pt-lg">
-            <q-select
-              class="col-6 q-pt-none" label="Anchor Panel" emit-value map-options
-              v-model="cfg.anchor" @update:model-value="v => putdb({ anchor: v })"
-              :options="panoRefAlignOptions"
-            />
-            <q-select
-              class="col-6 q-pt-none" label="to Reference Position" emit-value map-options
-              v-model="cfg.ref" @update:model-value="v => putdb({ ref: v })"
-              :options="panoRefTypeOptions"
-            />
-        </div>
-        <div v-if="cfg.ref==0" class="row q-col-gutter-lg q-pb-md items-center q-pt-md">
-            <div class="text-h7 col-3">Reference Position</div>
-            <q-input class="col-3" v-bind="bindField('r1', 'Azimuth', '°')" type="number" step="0.01" input-class="text-right" dense/>
-            <q-input class="col-3" v-bind="bindField('r2', 'Altitude', '°')" type="number" input-class="text-right" dense/>
-            <q-input class="col-3" v-bind="bindField('r3', 'Roll Angle', '°')" type="number" input-class="text-right" dense/>
-        </div>
-        <div v-if="cfg.ref==1" class="row q-col-gutter-lg q-pb-md items-center q-pt-sm">
-            <div class="text-h7 col-3">Reference</div>
-            <q-input class="col-3" v-bind="bindField('r1', 'Right Ascension', '°')" type="number" input-class="text-right" dense/>
-            <q-input class="col-3" v-bind="bindField('r2', 'Declination', '°')" type="number" input-class="text-right" dense/>
-            <q-input class="col-3" v-bind="bindField('r3', 'Position Angle', '°')" type="number" input-class="text-right" dense/>
-        </div>
-        <div v-if="cfg.ref==2" class="row q-col-gutter-lg q-pb-md items-center q-pt-sm">
-            <div class="text-h7 col-6">Reference</div>
-            <q-input class="col-6" v-bind="bindField('r1', 'Orbital ID')" type="number" input-class="text-right" dense/>
-        </div>
-        <div class="text-h6 q-pt-lg">Panel Navigation</div>
-        <div class="col text-caption text-grey-6 q-pb-none">
-          Click a panel number to slew the mount to the corresponding panel position.
-        </div>
+  <q-card flat bordered class="q-pa-md full-width">
+    <div class="text-h6">Panorama Grid Layout</div>
+    <div class="row q-col-gutter-lg items-center q-pt-sm">
+      <q-input
+        class="col-2"
+        v-bind="bindField('cols', 'Columns')"
+        type="number"
+        input-class="text-right"
+        dense
+      />
+      <q-input
+        class="col-2"
+        v-bind="bindField('rows', 'Rows')"
+        type="number"
+        input-class="text-right"
+        dense
+      />
+      <div class="col-1"></div>
+      <q-btn
+        class="col-1 self-end q-pt-sm"
+        size="md"
+        text-color="grey-6"
+        flat
+        rounded
+        dense
+        icon="mdi-calculator"
+      >
+        <q-popup-proxy><PanoCalculator class="" /></q-popup-proxy>
+      </q-btn>
+      <q-input
+        class="col-3"
+        v-bind="bindField('hstep', 'Horizontal Step', '°')"
+        type="number"
+        input-class="text-right"
+        dense
+      />
+      <q-input
+        class="col-3"
+        v-bind="bindField('vstep', 'Vertical Step', '°')"
+        type="number"
+        input-class="text-right"
+        dense
+      />
+    </div>
+    <div class="row q-col-gutter-lg items-center q-pt-lg">
+      <q-select
+        class="col-3 q-pt-none"
+        label="Panel Order"
+        emit-value
+        map-options
+        v-model="cfg.order"
+        @update:model-value="(v) => putdb({ order: v })"
+        :options="panoOrderOptions"
+      />
+      <q-select
+        class="col-5 q-pt-none"
+        label="Orientation and Tracking"
+        emit-value
+        map-options
+        v-model="cfg.track"
+        @update:model-value="(v) => putdb({ track: v })"
+        :options="panoTrackingOptions"
+      />
+      <q-select
+        class="col-4 q-pt-none"
+        label="Starting Panel"
+        emit-value
+        map-options
+        v-model="cfg.startpos"
+        @update:model-value="(v) => putdb({ startpos: v })"
+        :options="panoStartingPositionOptions"
+      />
+    </div>
+    <div class="text-h6 q-pt-lg">Panorama Grid Positioning</div>
+    <div class="row q-col-gutter-lg items-center q-pt-lg">
+      <q-select
+        class="col-6 q-pt-none"
+        label="Anchor Panel"
+        emit-value
+        map-options
+        v-model="cfg.anchor"
+        @update:model-value="(v) => putdb({ anchor: v })"
+        :options="panoRefAlignOptions"
+      />
+      <q-select
+        class="col-6 q-pt-none"
+        label="to Reference Position"
+        emit-value
+        map-options
+        v-model="cfg.ref"
+        @update:model-value="(v) => putdb({ ref: v })"
+        :options="panoRefTypeOptions"
+      />
+    </div>
+    <div v-if="cfg.ref == 0" class="row q-col-gutter-lg q-pb-md items-center q-pt-md">
+      <div class="text-h7 col-3">Reference Position</div>
+      <q-input
+        class="col-3"
+        v-bind="bindField('r1', 'Azimuth', '°')"
+        type="number"
+        step="0.01"
+        input-class="text-right"
+        dense
+      />
+      <q-input
+        class="col-3"
+        v-bind="bindField('r2', 'Altitude', '°')"
+        type="number"
+        input-class="text-right"
+        dense
+      />
+      <q-input
+        class="col-3"
+        v-bind="bindField('r3', 'Roll Angle', '°')"
+        type="number"
+        input-class="text-right"
+        dense
+      />
+    </div>
+    <div v-if="cfg.ref == 1" class="row q-col-gutter-lg q-pb-md items-center q-pt-sm">
+      <div class="text-h7 col-3">Reference</div>
+      <q-input
+        class="col-3"
+        v-bind="bindField('r1', 'Right Ascension', '°')"
+        type="number"
+        input-class="text-right"
+        dense
+      />
+      <q-input
+        class="col-3"
+        v-bind="bindField('r2', 'Declination', '°')"
+        type="number"
+        input-class="text-right"
+        dense
+      />
+      <q-input
+        class="col-3"
+        v-bind="bindField('r3', 'Position Angle', '°')"
+        type="number"
+        input-class="text-right"
+        dense
+      />
+    </div>
+    <div v-if="cfg.ref == 2" class="row q-col-gutter-lg q-pb-md items-center q-pt-sm">
+      <div class="text-h7 col-6">Reference</div>
+      <q-input
+        class="col-6"
+        v-bind="bindField('r1', 'Orbital ID')"
+        type="number"
+        input-class="text-right"
+        dense
+      />
+    </div>
+    <div class="text-h6 q-pt-lg">Panel Navigation</div>
+    <div class="col text-caption text-grey-6 q-pb-none">
+      Click a panel number to slew the mount to the corresponding panel position.
+    </div>
 
-        <PanoNavigation />
+    <PanoNavigation />
 
-        <div class="col text-caption text-grey-6 q-pt-md">
-         ✱ Indicates the next panel in the panorama sequence
-        </div>
-        <div class="col text-caption text-grey-6 ">
-        ⚓ Which part of the mosaic to be placed at the reference position
-        </div>
-              <div class="row text-grey-6">
-                <q-toggle v-bind="bindField('show_panels', 'Show Panel Navigation on Main Dashboard')"/>
-              </div>
-
-
-      </q-card>
+    <div class="col text-caption text-grey-6 q-pt-md">
+      ✱ Indicates the next panel in the panorama sequence
+    </div>
+    <div class="col text-caption text-grey-6">
+      ⚓ Which part of the mosaic to be placed at the reference position
+    </div>
+    <div class="row text-grey-6">
+      <q-toggle v-bind="bindField('show_panels', 'Show Panel Navigation on Main Dashboard')" />
+    </div>
+  </q-card>
 </template>
 
 <script setup lang="ts">
 // import axios from 'axios'
-import { onMounted, computed } from 'vue'
+import { onMounted, computed } from 'vue';
 import { useConfigStore } from 'stores/config';
 import { useDeviceStore } from 'src/stores/device';
-import { debounce } from 'quasar'
-import PanoNavigation from 'src/components/PanoNavigation.vue'
-import PanoCalculator from 'src/components/PanoCalculator.vue'
+import { debounce } from 'quasar';
+import PanoNavigation from 'src/components/PanoNavigation.vue';
+import PanoCalculator from 'src/components/PanoCalculator.vue';
 
-const dev = useDeviceStore()
-const cfg = useConfigStore()
+const dev = useDeviceStore();
+const cfg = useConfigStore();
 // cfg properties {"cols":5, "rows":3, "hstep": 50, "vstep": 30, "order":2, "track":2, "anchor":3, "ref":0, "r1":180, "r2":30, "r3":10, "panel":2 }
 
 const panoTrackingOptions = [
@@ -93,25 +189,30 @@ const panoTrackingOptions = [
   { label: 'Sky - Horizon-Locked', value: 1 },
   { label: 'Sky - Celestrial', value: 2 },
   // { label: 'Sky - Orbital', value: 3 },
-]
+];
 
 const panoOrderOptions = [
   { label: 'Row - Major', value: 0 },
   { label: 'Column - Major', value: 1 },
   { label: 'Serpentine', value: 2 },
-]
+];
 
+const panoStartingPositionOptions = [
+  { label: 'Top Left', value: 'tl' },
+  { label: 'Top Right', value: 'tr' },
+  { label: 'Bottom Left', value: 'bl' },
+  { label: 'Bottom Right', value: 'br' },
+];
 
 const panoRefTypeOptions = [
   { label: 'Az/Alt Point', value: 0 },
   // { label: 'RA/Dec Point', value: 1 },
   // { label: 'Orbital Element', value: 2 },
   { label: 'Current Orientation', value: 3 },
-]
-
+];
 
 const panoRefAlignOptions = computed(() => {
-  const panelCount =   Number(cfg.rows ?? 1) * Number(cfg.cols ?? 3)
+  const panelCount = Number(cfg.rows ?? 1) * Number(cfg.cols ?? 3);
   const options = [{ label: 'Whole Mosaic', value: 0 }];
   for (let i = 1; i <= panelCount; i++) {
     options.push({ label: `Panel ${i}`, value: i });
@@ -119,12 +220,11 @@ const panoRefAlignOptions = computed(() => {
   return options;
 });
 
-
 onMounted(async () => {
-  const shouldFetch =  dev.restAPIConnected && dev.restAPIConnectedAt &&cfg.fetchedAt < dev.restAPIConnectedAt
-  if (shouldFetch) await cfg.configFetch()
-})
-
+  const shouldFetch =
+    dev.restAPIConnected && dev.restAPIConnectedAt && cfg.fetchedAt < dev.restAPIConnectedAt;
+  if (shouldFetch) await cfg.configFetch();
+});
 
 function bindField(key: string, label: string, suffix?: string) {
   /**
@@ -138,9 +238,9 @@ function bindField(key: string, label: string, suffix?: string) {
    * @returns A binding object compatible with Quasar input components
    */
   // @ts-expect-error: dynamic key access on cfg
-  const val = cfg[key]
-  const type = typeof val
-  const isValid = ['string', 'number', 'boolean'].includes(type)
+  const val = cfg[key];
+  const type = typeof val;
+  const isValid = ['string', 'number', 'boolean'].includes(type);
   return {
     label,
     ...(suffix ? { suffix } : {}),
@@ -148,22 +248,15 @@ function bindField(key: string, label: string, suffix?: string) {
     'onUpdate:modelValue': (v: string | number | boolean | null) => {
       if (v !== null && isValid) {
         // @ts-expect-error: dynamic key assignment
-        cfg[key] = v
-        const payload = { [key]: v }
-        put(payload)
+        cfg[key] = v;
+        const payload = { [key]: v };
+        put(payload);
       }
-    }
-  }
+    },
+  };
 }
 
-
-
-
-
-// debounced payload key/values (a) sent to Alpaca Server and (b) patched into cfg store 
-const put = debounce((payload) => cfg.configUpdate(payload), 5)     // fast put for toggles
-const putdb = debounce((payload) => cfg.configUpdate(payload), 500) // slow put for input text
-
-
+// debounced payload key/values (a) sent to Alpaca Server and (b) patched into cfg store
+const put = debounce((payload) => cfg.configUpdate(payload), 5); // fast put for toggles
+const putdb = debounce((payload) => cfg.configUpdate(payload), 500); // slow put for input text
 </script>
-
