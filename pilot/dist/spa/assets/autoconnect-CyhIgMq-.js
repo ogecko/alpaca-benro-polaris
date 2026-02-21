76239c581c08a45c9193abd29e4d64694bf9ac43
David Morrison <jdmorriso@gmail.com>
2026-02-21 12:41:05 +1100

    fix(pilot): Rebuild Pilot to pickup axios upgrade and AboutPage

---
+++ pilot/dist/spa/assets/autoconnect-CyhIgMq-.js
@@ -0,0 +1,1 @@
+import{u as o}from"./device-CPieVB3u.js";import"./index-CIAzChoj.js";const c=async()=>{const t=o();t.alpacaHost&&t.restAPIPort&&await t.connectRestAPI()};export{c as default};
---
+++ pilot/dist/spa/assets/use-quasar-BMgZ0nZl.js
@@ -0,0 +1,1 @@
+import{j as a,aQ as r}from"./index-CIAzChoj.js";function u(){return a(r)}export{u};
