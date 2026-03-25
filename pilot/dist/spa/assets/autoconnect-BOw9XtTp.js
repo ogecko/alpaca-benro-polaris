0e29d2b64d52783ae0853ec351908690c3c81fe1
David Morrison <jdmorriso@gmail.com>
2026-03-25 18:46:55 +1100

    fix(pilot): Rebuild Pilot to pickup - Driver Restart; Gimbal Lock Status; Pano Recenter and Copy; Arc-sec on Charts

--- pilot/dist/spa/assets/autoconnect-BOw9XtTp.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./device-Dw_OFC3o.js";import"./index-BgVtxCI5.js";const c=async()=>{const t=o();t.alpacaHost&&t.restAPIPort&&await t.connectRestAPI()};export{c as default};
--- pilot/dist/spa/assets/use-quasar-D-A0k5VO.js
+++
@@ -1,1 +0,0 @@
-import{j as a,aQ as r}from"./index-BgVtxCI5.js";function u(){return a(r)}export{u};
