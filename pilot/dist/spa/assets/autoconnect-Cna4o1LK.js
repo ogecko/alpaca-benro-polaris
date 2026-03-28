211e1d49b0526d9e5710b2a358cbbe36b633eef0
David Morrison <jdmorriso@gmail.com>
2026-03-28 23:09:46 +1100

    fix(pilot): rebuild pilot to pick up new settings for PEC

--- pilot/dist/spa/assets/autoconnect-Cna4o1LK.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./device-DdMm99jM.js";import"./index-BcidEADh.js";const c=async()=>{const t=o();t.alpacaHost&&t.restAPIPort&&await t.connectRestAPI()};export{c as default};
---
+++ pilot/dist/spa/assets/use-quasar-Cha8G093.js
@@ -0,0 +1,1 @@
+import{j as a,aQ as r}from"./index-BMcqqmLu.js";function u(){return a(r)}export{u};
