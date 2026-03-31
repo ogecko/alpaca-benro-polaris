0ddf0810b90e95c1820562572240a4fa239ae6b1
David Morrison <jdmorriso@gmail.com>
2026-03-31 16:47:59 +1100

    fix(pilot): Rebuild Pilot to pickup Advanced Optimal Sidereal Tracking settings

---
+++ pilot/dist/spa/assets/autoconnect-D4__sdoz.js
@@ -0,0 +1,1 @@
+import{u as o}from"./device-DzxLmb-4.js";import"./index-DthM1SRJ.js";const c=async()=>{const t=o();t.alpacaHost&&t.restAPIPort&&await t.connectRestAPI()};export{c as default};
---
+++ pilot/dist/spa/assets/use-quasar-Da_X5tqv.js
@@ -0,0 +1,1 @@
+import{j as a,aQ as r}from"./index-DthM1SRJ.js";function u(){return a(r)}export{u};
