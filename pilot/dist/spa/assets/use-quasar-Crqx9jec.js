2195c3f5ca7ad71f63bd8cc47a35ee8da619c797
David Morrison <jdmorriso@gmail.com>
2026-03-31 23:48:35 +1100

    fix(pilot): rebuild Pilot for Local Gausian Alignment

---
+++ pilot/dist/spa/assets/use-quasar-Crqx9jec.js
@@ -0,0 +1,1 @@
+import{j as a,aQ as r}from"./index-BIhtakZC.js";function u(){return a(r)}export{u};
--- pilot/dist/spa/assets/use-quasar-Da_X5tqv.js
+++
@@ -1,1 +0,0 @@
-import{j as a,aQ as r}from"./index-DthM1SRJ.js";function u(){return a(r)}export{u};
