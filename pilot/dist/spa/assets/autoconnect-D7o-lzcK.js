b2f4b1b2d395e9f732c5103874476d04d466d469
David Morrison <jdmorriso@gmail.com>
2026-04-09 12:15:00 +1000

    fix(pilot): Rebuild Pilot to pickup kinematics page Speed Controller section

--- pilot/dist/spa/assets/autoconnect-D7o-lzcK.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./device-pxnX0rIc.js";import"./index-DgjTC43m.js";const c=async()=>{const t=o();t.alpacaHost&&t.restAPIPort&&await t.connectRestAPI()};export{c as default};
---
+++ pilot/dist/spa/assets/use-quasar-BzO5KCh5.js
@@ -0,0 +1,1 @@
+import{j as a,aX as r}from"./index-Cr8K4QD5.js";function u(){return a(r)}export{u};
