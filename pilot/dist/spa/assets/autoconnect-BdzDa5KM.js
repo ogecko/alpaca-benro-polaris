357abce09f76dd83bdf6e1bcd8899de0fe3d698a
David Morrison <jdmorriso@gmail.com>
2026-04-19 16:42:39 +1000

    fix(pilot): Rebuild Pilot to pickup revised kinematics page, scale limits for alt, roll fabs

--- pilot/dist/spa/assets/autoconnect-BdzDa5KM.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./device-yK2lOR8m.js";import"./index-CWw_CSZG.js";const c=async()=>{const t=o();t.alpacaHost&&t.restAPIPort&&await t.connectRestAPI()};export{c as default};
---
+++ pilot/dist/spa/assets/autoconnect-CdzQBR6x.js
@@ -0,0 +1,1 @@
+import{u as o}from"./device-ih5Fh7F7.js";import"./index-Dn7s6P7Z.js";const c=async()=>{const t=o();t.alpacaHost&&t.restAPIPort&&await t.connectRestAPI()};export{c as default};
