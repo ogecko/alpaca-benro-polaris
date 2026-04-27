6252587f4b5e8df92a25626455154835ff7282a2
David Morrison <jdmorriso@gmail.com>
2026-04-28 09:28:34 +1000

    fix(518): suppress error message from uvicorn when connection is being closed

--- driver/log.py
+++ driver/log.py
@@ -11,6 +11,11 @@ logger = None
 
 _log_queue_listener: logging.handlers.QueueListener = None  # keep reference for shutdown
 
+class UvicornH11Filter(logging.Filter):
+    def filter(self, record):
+        return 'can\'t handle event type Response when role=SERVER and state=CLOSED' not in record.getMessage()
+
+
 def init_logging():
     try:
         logging.basicConfig(level=Config.log_level)
@@ -43,6 +48,10 @@ def init_logging():
             file_handler.setFormatter(formatter)
             file_handler.doRollover()
             blocking_handlers.append(file_handler)
+
+        # supress known UvicornH11 error message which is harmless but ugly
+        logging.getLogger('uvicorn.error').addFilter(UvicornH11Filter())
+        logging.getLogger('uvicorn.access').addFilter(UvicornH11Filter())
 
         # Remove all existing handlers from root logger
         root_logger.handlers.clear()
