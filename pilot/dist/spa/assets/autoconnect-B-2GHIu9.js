b71e51b90d972be6d6f1249a62b26054be692746
David Morrison <jdmorriso@gmail.com>
2026-04-27 17:02:33 +1000

    fix(pilot): Rebuild Pilot to pickup corrected RMS Error on charts

---
+++ pilot/dist/spa/assets/autoconnect-B-2GHIu9.js
@@ -0,0 +1,1 @@
+import{u as o}from"./device-DLnjXBri.js";import"./index-Cyb19g4f.js";const c=async()=>{const t=o();t.alpacaHost&&t.restAPIPort&&await t.connectRestAPI()};export{c as default};
---
+++ pilot/dist/spa/assets/use-quasar-yJhMi1XD.js
@@ -0,0 +1,1 @@
+import{j as a,aX as r}from"./index-Cyb19g4f.js";function u(){return a(r)}export{u};
