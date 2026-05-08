4b1d5d6b1b02a30267e27f1cdb128db4f6c920d0
David Morrison <jdmorriso@gmail.com>
2026-05-08 13:53:42 +1000

    fix(pilot): Rebuild pilot to pickup fix for Settings Page

--- pilot/dist/spa/assets/autoconnect-COkfOvEk.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./device-DX_BfCGB.js";import"./index-CfD5XB5h.js";const c=async()=>{const t=o();t.alpacaHost&&t.restAPIPort&&await t.connectRestAPI()};export{c as default};
--- pilot/dist/spa/assets/use-quasar-BVkkGn7O.js
+++
@@ -1,1 +0,0 @@
-import{j as a,aX as r}from"./index-CfD5XB5h.js";function u(){return a(r)}export{u};
