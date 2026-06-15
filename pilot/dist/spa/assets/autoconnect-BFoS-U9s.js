87e79e62c9f02536c923facfb6c196af06e52512
David Morrison <jdmorriso@gmail.com>
2026-06-15 16:37:06 +1000

    fix(pilot): Rebuild Pilot to pickup changes from automated code review scan

---
+++ pilot/dist/spa/assets/autoconnect-BFoS-U9s.js
@@ -0,0 +1,1 @@
+import{t as e}from"./device-CcV6fZkn.js";var t=async()=>{let t=e();t.alpacaHost&&t.restAPIPort&&await t.connectRestAPI()};export{t as default};
--- pilot/dist/spa/assets/autoconnect-DaCu8aZx.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./device-C8C81YcH.js";var t=async()=>{let t=e();t.alpacaHost&&t.restAPIPort&&await t.connectRestAPI()};export{t as default};
