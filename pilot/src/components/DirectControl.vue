6828f1e198644ca27c15323d24bc2e98b89816bd
David Morrison <jdmorriso@gmail.com>
2026-07-05 16:52:16 +1000

    fix(move): use MoveButton for all direct axis controls

--- pilot/src/components/DirectControl.vue
+++ pilot/src/components/DirectControl.vue
@@ -4,24 +4,27 @@
 
       <!-- Row 1 -->
       <q-btn outline size="xl" color="primary" icon="mdi-home" @click="sendCommand('home')" />
-      <q-btn rounded size="xl" color="primary" icon="mdi-arrow-down-left-bold" @mousedown="startMove('ccw')" @mouseup="stopMove" @mouseleave="stopMove" />
-      <q-btn rounded size="xl" color="primary" icon="mdi-arrow-up-bold" @mousedown="startMove('north')" @mouseup="stopMove" @mouseleave="stopMove" />
-      <q-btn rounded size="xl" color="primary" icon="mdi-arrow-down-right-bold" @mousedown="startMove('cw')" @mouseup="stopMove" @mouseleave="stopMove" />
+      <MoveButton rounded size="xl" :opacity="0.8" icon="mdi-arrow-down-left-bold" color="positive" upcolor="primary" iupcolor="white" @push="e=>onMove({...e, axis:2, dir: -1})"/>
+      <MoveButton rounded size="xl" :opacity="0.8" icon="mdi-arrow-up-bold" color="positive" upcolor="primary" iupcolor="white" @push="e=>onMove({...e, axis:1, dir: +1})"/>
+      <MoveButton rounded size="xl" :opacity="0.8" icon="mdi-arrow-down-right-bold" color="positive" upcolor="primary" iupcolor="white" @push="e=>onMove({...e, axis:2, dir: +1})"/>
 
       <!-- Row 2 -->
       <q-btn outline size="xl" color="primary" icon="mdi-parking" @click="sendCommand('park')" />
-      <q-btn rounded size="xl" color="primary" icon="mdi-arrow-left-bold" @mousedown="startMove('west')" @mouseup="stopMove" @mouseleave="stopMove" />
-      <div class="knob-container"> 
+      <div class="btn-container"> 
+      <MoveButton rounded size="xl" :opacity="0.8" icon="mdi-arrow-left-bold" color="positive" upcolor="primary" iupcolor="white" @push="e=>onMove({...e, axis:0, dir: -1})"/>
+        <div  class="text-caption text-grey-8 q-mt-xs">-ve Azimuth</div>
+      </div>
+        <div class="btn-container"> 
         <q-knob v-model="speed" :min="0" :inner-min="1" :inner-max="9" :max="10" size="80px" show-value color="positive" track-color="grey-7" /> 
         <div  class="text-caption text-positive q-mt-xs">Speed</div>
       </div>
-      <q-btn rounded size="xl" color="primary" icon="mdi-arrow-right-bold" @mousedown="startMove('east')" @mouseup="stopMove" @mouseleave="stopMove" />
+      <MoveButton rounded size="xl" :opacity="0.8" icon="mdi-arrow-right-bold" color="positive" upcolor="primary" iupcolor="white" @push="e=>onMove({...e, axis:0, dir: +1})"/>
 
 
       <!-- Row 3 -->
       <q-btn outline size="xl" color="negative" icon="mdi-stop" @click="sendCommand('stop')" />
       <q-btn outline size="xl" color="secondary" icon="mdi-star-shooting-outline" @click="sendCommand('track')" />
-      <q-btn rounded size="xl" color="primary" icon="mdi-arrow-down-bold" @mousedown="startMove('south')" @mouseup="stopMove" @mouseleave="stopMove" />
+      <MoveButton rounded size="xl" :opacity="0.8" icon="mdi-arrow-down-bold" color="positive" upcolor="primary" iupcolor="white" @push="e=>onMove({...e, axis:1, dir: -1})"/>
       <q-btn outline size="xl" color="primary" icon="mdi-format-vertical-align-center" @click="sendCommand('level')" />
     </div>
   </div>
@@ -29,23 +32,21 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import MoveButton from 'src/components//MoveButton.vue'
 
 const speed = ref(5)
+
+function onMove(  payload: { isPressed: boolean, axis: number, dir: number }) {
+  const velocity = payload.isPressed ? speed.value * payload.dir : 0
+  console.log(payload.axis, velocity)
+}
+
 
 function sendCommand(command:string) {
   console.log(command)
   // emit to mount driver
 }
 
-function startMove(direction:string) {
-  console.log('move', direction, 'speed', speed.value)
-  // start slew
-}
-
-function stopMove() {
-  console.log('stop movement')
-  // stop slew
-}
 </script>
 
 <style scoped>
@@ -64,7 +65,7 @@ function stopMove() {
   justify-items: center;
 }
 
-.knob-container {
+.btn-container {
   display: flex;
   flex-direction: column;
   align-items: center;
