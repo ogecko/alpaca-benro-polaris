ff0fdc35009d8bf76859ecb12c532fbc6c169ecc
David Morrison <jdmorriso@gmail.com>
2026-07-17 15:37:35 +1000

    fix(move): revised joystick panel request for feedback

--- pilot/src/components/DirectControl.vue
+++ pilot/src/components/DirectControl.vue
@@ -3,29 +3,37 @@
     <div class="control-grid">
 
       <!-- Row 1 -->
+      <q-btn outline size="xl" color="primary" icon="mdi-arrow-all" @click="sendCommand('coord')" />
+      <q-btn outline size="xl" color="primary" icon="mdi-telescope" @click="sendCommand('coord')" />
+      <q-btn outline split size="xl" color="secondary" icon="mdi-star-shooting-outline" @click="sendCommand('track')" />
       <q-btn outline size="xl" color="primary" icon="mdi-home" @click="sendCommand('home')" />
+      <q-btn outline size="xl" color="primary" icon="mdi-stop" @click="sendCommand('stop')" />
+      <!-- Row 2 -->
+      <q-btn outline size="xl" color="primary" icon="mdi-move-resize-variant" @click="sendCommand('Goto')" />
       <MoveButton rounded size="xl" :opacity="0.8" icon="mdi-arrow-down-left-bold" color="positive" upcolor="primary" iupcolor="white" @push="e=>onMove({...e, axis:2, dir: -1})"/>
       <MoveButton rounded size="xl" :opacity="0.8" icon="mdi-arrow-up-bold" color="positive" upcolor="primary" iupcolor="white" @push="e=>onMove({...e, axis:1, dir: +1})"/>
       <MoveButton rounded size="xl" :opacity="0.8" icon="mdi-arrow-down-right-bold" color="positive" upcolor="primary" iupcolor="white" @push="e=>onMove({...e, axis:2, dir: +1})"/>
-      <q-btn outline size="xl" color="green" label="Sync" @click="sendCommand('sync')" />
-
-      <!-- Row 2 -->
       <q-btn outline size="xl" color="primary" icon="mdi-parking" @click="sendCommand('park')" />
+
+      <!-- Row 3 -->
+      <div class="btn-container"> 
+        <q-btn outline split size="xl" color="green" icon="mdi-sync" @click="sendCommand('sync')" />
+        <div  class="text-caption text-positive q-mt-xs">Artares</div>
+      </div>
       <MoveButton rounded size="xl" :opacity="0.8" icon="mdi-arrow-left-bold" color="positive" upcolor="primary" iupcolor="white" @push="e=>onMove({...e, axis:0, dir: -1})"/>
       <div class="btn-container"> 
         <q-knob v-model="speed" :min="0" :inner-min="1" :inner-max="9" :max="10" size="80px" show-value color="positive" track-color="grey-7" /> 
         <div  class="text-caption text-positive q-mt-xs">Speed</div>
       </div>
       <MoveButton rounded size="xl" :opacity="0.8" icon="mdi-arrow-right-bold" color="positive" upcolor="primary" iupcolor="white" @push="e=>onMove({...e, axis:0, dir: +1})"/>
-      <q-btn outline size="xl" color="primary" label="Horz" @click="sendCommand('level')" />
+      <q-btn outline size="xl" color="primary" icon="mdi-cryengine" @click="sendCommand('glat0')" />
 
-
-      <!-- Row 3 -->
-      <q-btn outline size="xl" color="primary" icon="mdi-stop" @click="sendCommand('stop')" />
-      <q-btn outline size="xl" color="secondary" icon="mdi-star-shooting-outline" @click="sendCommand('track')" />
+      <!-- Row 4 -->
+      <q-btn outline size="xl" color="primary" icon="mdi-globe-model" @click="sendCommand('mpa')" />
+      <q-btn outline size="xl" color="primary" icon="mdi-format-align-middle" @click="sendCommand('level')" />
       <MoveButton rounded size="xl" :opacity="0.8" icon="mdi-arrow-down-bold" color="positive" upcolor="primary" iupcolor="white" @push="e=>onMove({...e, axis:1, dir: -1})"/>
-      <q-btn outline size="xl" color="primary" label="RA-2" @click="sendCommand('rap2')" />
-      <q-btn outline size="xl" color="primary" label="RA+2" @click="sendCommand('ram2')" />
+      <q-btn outline split size="xl" color="primary"  label="RA-2" @click="sendCommand('ram2')" />
+      <q-btn outline split size="xl" color="primary"  label="RA+2" @click="sendCommand('rap2')" />
     </div>
   </div>
 </template>
@@ -60,7 +68,7 @@ function sendCommand(command:string) {
   display: grid;
   grid-template-columns: repeat(5, 100px);
   grid-template-rows: repeat(3, 100px);
-  gap: 0px;
+  gap: 6px;
   align-items: center;
   justify-items: center;
 }
