71ec129d6ee9c1a87c767a606f3d8aa9302397be
David Morrison <jdmorriso@gmail.com>
2026-08-01 14:15:06 +1000

    fix(pilot): Rebuild pilot to pick up status ifo icon and new Sync Guiding Tests on PID Tuning page

---
+++ pilot/dist/spa/assets/autoconnect-CLH_giAQ.js
@@ -0,0 +1,1 @@
+import{t as e}from"./device-CZ3F65JE.js";var t=async()=>{let t=e();t.alpacaHost&&t.restAPIPort&&await t.connectRestAPI()};export{t as default};
--- pilot/dist/spa/assets/autoconnect-iWPIIBrk.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./device-CgQ2KiLo.js";var t=async()=>{let t=e();t.alpacaHost&&t.restAPIPort&&await t.connectRestAPI()};export{t as default};
